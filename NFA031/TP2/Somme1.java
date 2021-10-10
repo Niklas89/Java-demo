9c87cf1832a45b710326b870127bdaed98b4aed2
Niklas <niklas_edelstam@hotmail.com>
2021-10-10 09:45:16 +0200

    deleted

--- NFA031/TP2/Somme1.java
+++
@@ -1,9 +0,0 @@
-public class Somme1 {
-    public static void main (String [] arguments)       { 
-        int x =3;
-        int y =4;
-        int z;
-        z=x+y;
-        Terminal.ecrireStringln("la somme de "+ x + " et de " + y + " est  " + z);        
-       }
-}
--- NFA031/TP2/Somme2.java
+++
@@ -1,8 +0,0 @@
-public class Somme2 {
-    public static void main (String [] arguments)      { 
-         int x = 3;
-         int y = 4;
-         x= x+y;
-         Terminal.ecrireStringln("la somme de "+ x + " et de " + y + " est " + x);
-       }
-}
