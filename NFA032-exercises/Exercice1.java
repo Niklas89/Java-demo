2847f7a4236d6f86915fd22eb533ec382ce9e95f
Niklas <niklas_edelstam@hotmail.com>
2022-05-20 13:27:09 +0200

    modifs

--- NFA032-exercises/Exercice1.java
+++ NFA032-exercises/Exercice1.java
@@ -5,32 +5,49 @@ import java.util.function.BiFunction;
 
 public class Exercice1 {
 	
+	long count = 0;
+	
+	BiFunction<List<String>, String, Long> functionList = (maliste , monString) -> {
+		maliste.forEach(element -> {
+		    if(element.equals(monString)) {
+		    	count++;
+		    }  
+		});
+		return count;
+	};
+	
+	
 	private long count(List<String> list, String myString) {
 		
-		BiFunction<List<String>, String, Integer> functionList = (maliste , monString) -> {
-			int count = 0;
-			maliste.forEach(element -> {
-			    if (element.contains(monString)) {
-			        count++;
-			    }
-			});
-			return count;
-		};
-		
 		return functionList.apply(list, myString);
-		
 	}
 
 	public static void main(String[] args) {
 		
-		
 		List<String> list = List.of("hello", "world", "hello", "lambda");
 		
 		Exercice1 obj = new Exercice1();
-		
 	
 		System.out.println(obj.count(list, "hello"));
 
 	}
+	
+	
+	/*
+	private long count(List<String> list, String myString) {
+		
+		final long count[] = {0};
+		
+		BiFunction<List<String>, String, Long> functionList = (maliste , monString) -> {
+			maliste.forEach(element -> {
+			    if(element.equals(monString)) {
+			    	count[0]++;
+			    }  
+			});
+			return count[0];
+		};
+		
+		return functionList.apply(list, myString);
+	} */
 
 }
